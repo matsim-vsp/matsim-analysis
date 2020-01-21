ddf9f27b57029e0ecda1f4ac05ae2d54e58977a1
ikaddoura <kaddoura@vsp.tu-berlin.de>
2020-01-21 23:20:01 +0100

    matsim version number; analysis version number

--- src/main/java/org/matsim/analysis/MatsimAnalysis.java
+++ src/main/java/org/matsim/analysis/MatsimAnalysis.java
@@ -99,7 +99,7 @@ public class MatsimAnalysis {
 	// base case (optional)
 	private Scenario scenario0;
 	
-	private final String outputDirectoryName = "analysis-v2.5";
+	private final String outputDirectoryName = "analysis-v2.6";
 	private final String stageActivitySubString = "interaction";
 
 	public void run() {
