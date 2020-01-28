c006a9b7c2aa60ca7cfd1f444014444142b0d894
Janek Laudan <laudan@tu-berlin.de>
2020-01-28 14:14:41 +0100

    Add commandline args to example

--- src/main/java/org/matsim/analysis/AnalysisRunExample.java
+++ src/main/java/org/matsim/analysis/AnalysisRunExample.java
@@ -19,37 +19,47 @@
 
 package org.matsim.analysis;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class AnalysisRunExample {
 	private static final Logger log = Logger.getLogger(AnalysisRunExample.class);
-			
-	public static void main(String[] args) throws IOException {
-			
-		final String runDirectory = "/path-to-run-directory/";
-		final String runId = "run-id";		
-		final String modesString = "car,pt";
-		
+
+	/**
+	 * analysis can be invoked with commandline args. If any of the arguments are missing the hardcoded ones are taken.
+	 * Example:
+	 * "-runDir /path/to/run/directory -runId run-id -modes car,pt,bike,walk"
+	 *
+	 * @param args Command Line args. Possible parameters are -runDir, -runId, -modes
+	 */
+	public static void main(String[] args) {
+
+		Args arguments = new Args();
+		JCommander.newBuilder().addObject(arguments).build().parse(args);
+
+		final String runDirectory = arguments.runDirectory.isEmpty() ? "/path-to-run-directory/" : arguments.runDirectory;
+		final String runId = arguments.runId.isEmpty() ? "run-id" : arguments.runId;
+		final String modesString = arguments.modes.isEmpty() ? "car,pt" : arguments.modes;
+
 		Scenario scenario1 = loadScenario(runDirectory, runId);
-		
+
 		List<AgentFilter> filters1 = new ArrayList<>();
 		AgentAnalysisFilter filter1a = new AgentAnalysisFilter("A");
 		filter1a.preProcess(scenario1);
 		filters1.add(filter1a);
-				
-		List<String> modes = new ArrayList<>();
-		for (String mode : modesString.split(",")) {
-			modes.add(mode);
-		}
-		
+
+		List<String> modes = new ArrayList<>(Arrays.asList(modesString.split(",")));
+
 		MatsimAnalysis analysis = new MatsimAnalysis();
 		analysis.setScenario1(scenario1);
 		analysis.setAgentFilters(filters1);
@@ -59,25 +69,22 @@ public class AnalysisRunExample {
 	
 	private static Scenario loadScenario(String runDirectory, String runId) {
 		log.info("Loading scenario...");
-		
-		if (runDirectory == null || runDirectory.equals("") || runDirectory.equals("null")) {
-			return null;	
+
+		if (StringUtils.isBlank(runDirectory) || runDirectory.equals("null")) {
+			return null;
 		}
-		
+
 		if (!runDirectory.endsWith("/")) runDirectory = runDirectory + "/";
 
-		String networkFile;
-		String populationFile;
-		String configFile;
-		
-		configFile = runDirectory + runId + ".output_config.xml";	
-		networkFile = runId + ".output_network.xml.gz";
-		populationFile = runId + ".output_plans.xml.gz";
+		String configFile = runDirectory + runId + ".output_config.xml";
+		String networkFile = runId + ".output_network.xml.gz";
+		String populationFile = runId + ".output_plans.xml.gz";
 
 		Config config = ConfigUtils.loadConfig(configFile);
 
 		if (config.controler().getRunId() != null) {
-			if (!runId.equals(config.controler().getRunId())) throw new RuntimeException("Given run ID " + runId + " doesn't match the run ID given in the config file. Aborting...");
+			if (!runId.equals(config.controler().getRunId()))
+				throw new RuntimeException("Given run ID " + runId + " doesn't match the run ID given in the config file. Aborting...");
 		} else {
 			config.controler().setRunId(runId);
 		}
@@ -88,10 +95,21 @@ public class AnalysisRunExample {
 		config.vehicles().setVehiclesFile(null);
 		config.transit().setTransitScheduleFile(null);
 		config.transit().setVehiclesFile(null);
-		
+
 		return ScenarioUtils.loadScenario(config);
 	}
 
+	private static class Args {
+
+		@Parameter(names = {"-runDir", "-d"})
+		private String runDirectory = "";
+
+		@Parameter(names = {"-runId", "-r"})
+		private String runId = "";
+
+		@Parameter(names = {"-mode", "-m"})
+		private String modes = "";
+	}
 }
 		
 
