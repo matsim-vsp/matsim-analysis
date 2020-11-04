ab1a88d42855d1f9317b63659c3512aadc73b19e
aravindsasi <aravindsasidharan999@gmail.com>
2020-11-04 20:43:06 +0100

    Modified test class

--- src/test/java/org/matsim/analysis/RunsOverviewTest.java
+++ src/test/java/org/matsim/analysis/RunsOverviewTest.java
@@ -37,116 +37,123 @@ public class RunsOverviewTest {
 			for (CSVRecord csvRecord : csvParserRunOverview) {
 				for (CSVRecord csvRecordToCompare : csvParserRunOverviewToCompare) {
 
-					if (csvRecordToCompare.getRecordNumber() > 1) {
-
 //						String val1 = "0.22";
 //						String val2 = "1.22";
 //						Assert.assertArrayEquals("failure" + " fails", val1.toCharArray(),
 //								val2.toCharArray());
-						
-						Assert.assertArrayEquals(csvRecord.get(3)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(4).toCharArray(),
-								csvRecord.get(4).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(3)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(5).toCharArray(),
-								csvRecord.get(5).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(3)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(6).toCharArray(),
-								csvRecord.get(6).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(3)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(7).toCharArray(),
-								csvRecord.get(7).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(3)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(8).toCharArray(),
-								csvRecord.get(8).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(3)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(9).toCharArray(),
-								csvRecord.get(9).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(3)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(10).toCharArray(),
-								csvRecord.get(10).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(3)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(11).toCharArray(),
-								csvRecord.get(11).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(3)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(12).toCharArray(),
-								csvRecord.get(12).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(3)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(13).toCharArray(),
-								csvRecord.get(13).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(3)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(14).toCharArray(),
-								csvRecord.get(14).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(3)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(15).toCharArray(),
-								csvRecord.get(15).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(3)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(16).toCharArray(),
-								csvRecord.get(16).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(3)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(17).toCharArray(),
-								csvRecord.get(17).toCharArray());
+					String fileNameOne = (!(csvRecord.get(3).length() > 1) ? "Row" : csvRecord.get(3));
+					String runID = csvRecord.get(0);
 
-						Assert.assertArrayEquals(csvRecord.get(19)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(20).toCharArray(),
-								csvRecord.get(20).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(19)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(21).toCharArray(),
-								csvRecord.get(21).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(19)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(22).toCharArray(),
-								csvRecord.get(22).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(19)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(23).toCharArray(),
-								csvRecord.get(23).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(19)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(24).toCharArray(),
-								csvRecord.get(24).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(19)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(25).toCharArray(),
-								csvRecord.get(25).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(19)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(26).toCharArray(),
-								csvRecord.get(26).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(19)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(27).toCharArray(),
-								csvRecord.get(27).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(19)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(28).toCharArray(),
-								csvRecord.get(28).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(19)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(29).toCharArray(),
-								csvRecord.get(29).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(19)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(30).toCharArray(),
-								csvRecord.get(30).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(19)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(31).toCharArray(),
-								csvRecord.get(31).toCharArray());
+					Assert.assertArrayEquals(fileNameOne + " of " + runID + " does not match",
+							csvRecordToCompare.get(4).toCharArray(), csvRecord.get(4).toCharArray());
+					Assert.assertArrayEquals(fileNameOne + " of " + runID + " does not match",
+							csvRecordToCompare.get(5).toCharArray(), csvRecord.get(5).toCharArray());
+					Assert.assertArrayEquals(fileNameOne + " of " + runID + " does not match",
+							csvRecordToCompare.get(6).toCharArray(), csvRecord.get(6).toCharArray());
+					Assert.assertArrayEquals(fileNameOne + " of " + runID + " does not match",
+							csvRecordToCompare.get(7).toCharArray(), csvRecord.get(7).toCharArray());
+					Assert.assertArrayEquals(fileNameOne + " of " + runID + " does not match",
+							csvRecordToCompare.get(8).toCharArray(), csvRecord.get(8).toCharArray());
+					Assert.assertArrayEquals(fileNameOne + " of " + runID + " does not match",
+							csvRecordToCompare.get(9).toCharArray(), csvRecord.get(9).toCharArray());
+					Assert.assertArrayEquals(fileNameOne + " of " + runID + " does not match",
+							csvRecordToCompare.get(10).toCharArray(), csvRecord.get(10).toCharArray());
+					Assert.assertArrayEquals(fileNameOne + " of " + runID + " does not match",
+							csvRecordToCompare.get(11).toCharArray(), csvRecord.get(11).toCharArray());
+					Assert.assertArrayEquals(fileNameOne + " of " + runID + " does not match",
+							csvRecordToCompare.get(12).toCharArray(), csvRecord.get(12).toCharArray());
+					Assert.assertArrayEquals(fileNameOne + " of " + runID + " does not match",
+							csvRecordToCompare.get(13).toCharArray(), csvRecord.get(13).toCharArray());
+					Assert.assertArrayEquals(fileNameOne + " of " + runID + " does not match",
+							csvRecordToCompare.get(14).toCharArray(), csvRecord.get(14).toCharArray());
+					Assert.assertArrayEquals(fileNameOne + " of " + runID + " does not match",
+							csvRecordToCompare.get(15).toCharArray(), csvRecord.get(15).toCharArray());
+					Assert.assertArrayEquals(fileNameOne + " of " + runID + " does not match",
+							csvRecordToCompare.get(16).toCharArray(), csvRecord.get(16).toCharArray());
+					Assert.assertArrayEquals(fileNameOne + " of " + runID + " does not match",
+							csvRecordToCompare.get(17).toCharArray(), csvRecord.get(17).toCharArray());
 
-						Assert.assertArrayEquals(csvRecord.get(33)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(34).toCharArray(),
-								csvRecord.get(34).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(33)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(35).toCharArray(),
-								csvRecord.get(35).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(33)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(36).toCharArray(),
-								csvRecord.get(36).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(33)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(37).toCharArray(),
-								csvRecord.get(37).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(33)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(38).toCharArray(),
-								csvRecord.get(38).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(33)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(39).toCharArray(),
-								csvRecord.get(39).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(33)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(40).toCharArray(),
-								csvRecord.get(40).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(33)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(41).toCharArray(),
-								csvRecord.get(41).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(33)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(42).toCharArray(),
-								csvRecord.get(42).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(33)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(43).toCharArray(),
-								csvRecord.get(43).toCharArray());
+					String fileNameTwo = (!(csvRecord.get(19).length() > 1) ? "Row" : csvRecord.get(19));
 
-						Assert.assertArrayEquals(csvRecord.get(45)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(46).toCharArray(),
-								csvRecord.get(46).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(45)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(47).toCharArray(),
-								csvRecord.get(47).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(45)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(48).toCharArray(),
-								csvRecord.get(48).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(45)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(49).toCharArray(),
-								csvRecord.get(49).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(45)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(50).toCharArray(),
-								csvRecord.get(50).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(45)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(51).toCharArray(),
-								csvRecord.get(51).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(45)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(52).toCharArray(),
-								csvRecord.get(52).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(45)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(53).toCharArray(),
-								csvRecord.get(53).toCharArray());
+					Assert.assertArrayEquals(fileNameTwo + " of " + runID + " does not match",
+							csvRecordToCompare.get(20).toCharArray(), csvRecord.get(20).toCharArray());
+					Assert.assertArrayEquals(fileNameTwo + " of " + runID + " does not match",
+							csvRecordToCompare.get(21).toCharArray(), csvRecord.get(21).toCharArray());
+					Assert.assertArrayEquals(fileNameTwo + " of " + runID + " does not match",
+							csvRecordToCompare.get(22).toCharArray(), csvRecord.get(22).toCharArray());
+					Assert.assertArrayEquals(fileNameTwo + " of " + runID + " does not match",
+							csvRecordToCompare.get(23).toCharArray(), csvRecord.get(23).toCharArray());
+					Assert.assertArrayEquals(fileNameTwo + " of " + runID + " does not match",
+							csvRecordToCompare.get(24).toCharArray(), csvRecord.get(24).toCharArray());
+					Assert.assertArrayEquals(fileNameTwo + " of " + runID + " does not match",
+							csvRecordToCompare.get(25).toCharArray(), csvRecord.get(25).toCharArray());
+					Assert.assertArrayEquals(fileNameTwo + " of " + runID + " does not match",
+							csvRecordToCompare.get(26).toCharArray(), csvRecord.get(26).toCharArray());
+					Assert.assertArrayEquals(fileNameTwo + " of " + runID + " does not match",
+							csvRecordToCompare.get(27).toCharArray(), csvRecord.get(27).toCharArray());
+					Assert.assertArrayEquals(fileNameTwo + " of " + runID + " does not match",
+							csvRecordToCompare.get(28).toCharArray(), csvRecord.get(28).toCharArray());
+					Assert.assertArrayEquals(fileNameTwo + " of " + runID + " does not match",
+							csvRecordToCompare.get(29).toCharArray(), csvRecord.get(29).toCharArray());
+					Assert.assertArrayEquals(fileNameTwo + " of " + runID + " does not match",
+							csvRecordToCompare.get(30).toCharArray(), csvRecord.get(30).toCharArray());
+					Assert.assertArrayEquals(fileNameTwo + " of " + runID + " does not match",
+							csvRecordToCompare.get(31).toCharArray(), csvRecord.get(31).toCharArray());
 
-						Assert.assertArrayEquals(csvRecord.get(55)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(56).toCharArray(),
-								csvRecord.get(56).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(55)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(57).toCharArray(),
-								csvRecord.get(57).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(55)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(58).toCharArray(),
-								csvRecord.get(58).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(55)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(59).toCharArray(),
-								csvRecord.get(59).toCharArray());
-						Assert.assertArrayEquals(csvRecord.get(55)+" of "+csvRecord.get(0) + " does not match", csvRecordToCompare.get(60).toCharArray(),
-								csvRecord.get(60).toCharArray());
-					}
+					String fileNameThree = (!(csvRecord.get(33).length() > 1) ? "Row" : csvRecord.get(33));
+
+					Assert.assertArrayEquals(fileNameThree + " of " + runID + " does not match",
+							csvRecordToCompare.get(34).toCharArray(), csvRecord.get(34).toCharArray());
+					Assert.assertArrayEquals(fileNameThree + " of " + runID + " does not match",
+							csvRecordToCompare.get(35).toCharArray(), csvRecord.get(35).toCharArray());
+					Assert.assertArrayEquals(fileNameThree + " of " + runID + " does not match",
+							csvRecordToCompare.get(36).toCharArray(), csvRecord.get(36).toCharArray());
+					Assert.assertArrayEquals(fileNameThree + " of " + runID + " does not match",
+							csvRecordToCompare.get(37).toCharArray(), csvRecord.get(37).toCharArray());
+					Assert.assertArrayEquals(fileNameThree + " of " + runID + " does not match",
+							csvRecordToCompare.get(38).toCharArray(), csvRecord.get(38).toCharArray());
+					Assert.assertArrayEquals(fileNameThree + " of " + runID + " does not match",
+							csvRecordToCompare.get(39).toCharArray(), csvRecord.get(39).toCharArray());
+					Assert.assertArrayEquals(fileNameThree + " of " + runID + " does not match",
+							csvRecordToCompare.get(40).toCharArray(), csvRecord.get(40).toCharArray());
+					Assert.assertArrayEquals(fileNameThree + " of " + runID + " does not match",
+							csvRecordToCompare.get(41).toCharArray(), csvRecord.get(41).toCharArray());
+					Assert.assertArrayEquals(fileNameThree + " of " + runID + " does not match",
+							csvRecordToCompare.get(42).toCharArray(), csvRecord.get(42).toCharArray());
+					Assert.assertArrayEquals(fileNameThree + " of " + runID + " does not match",
+							csvRecordToCompare.get(43).toCharArray(), csvRecord.get(43).toCharArray());
+
+					String fileNameFour = (!(csvRecord.get(45).length() > 1) ? "Row" : csvRecord.get(45));
+
+					Assert.assertArrayEquals(fileNameFour + " of " + runID + " does not match",
+							csvRecordToCompare.get(46).toCharArray(), csvRecord.get(46).toCharArray());
+					Assert.assertArrayEquals(fileNameFour + " of " + runID + " does not match",
+							csvRecordToCompare.get(47).toCharArray(), csvRecord.get(47).toCharArray());
+					Assert.assertArrayEquals(fileNameFour + " of " + runID + " does not match",
+							csvRecordToCompare.get(48).toCharArray(), csvRecord.get(48).toCharArray());
+					Assert.assertArrayEquals(fileNameFour + " of " + runID + " does not match",
+							csvRecordToCompare.get(49).toCharArray(), csvRecord.get(49).toCharArray());
+					Assert.assertArrayEquals(fileNameFour + " of " + runID + " does not match",
+							csvRecordToCompare.get(50).toCharArray(), csvRecord.get(50).toCharArray());
+					Assert.assertArrayEquals(fileNameFour + " of " + runID + " does not match",
+							csvRecordToCompare.get(51).toCharArray(), csvRecord.get(51).toCharArray());
+					Assert.assertArrayEquals(fileNameFour + " of " + runID + " does not match",
+							csvRecordToCompare.get(52).toCharArray(), csvRecord.get(52).toCharArray());
+					Assert.assertArrayEquals(fileNameFour + " of " + runID + " does not match",
+							csvRecordToCompare.get(53).toCharArray(), csvRecord.get(53).toCharArray());
+
+					String fileNameFive = (!(csvRecord.get(55).length() > 1) ? "Row" : csvRecord.get(55));
+
+					Assert.assertArrayEquals(fileNameFive + " of " + runID + " does not match",
+							csvRecordToCompare.get(56).toCharArray(), csvRecord.get(56).toCharArray());
+					Assert.assertArrayEquals(fileNameFive + " of " + runID + " does not match",
+							csvRecordToCompare.get(57).toCharArray(), csvRecord.get(57).toCharArray());
+					Assert.assertArrayEquals(fileNameFive + " of " + runID + " does not match",
+							csvRecordToCompare.get(58).toCharArray(), csvRecord.get(58).toCharArray());
+					Assert.assertArrayEquals(fileNameFive + " of " + runID + " does not match",
+							csvRecordToCompare.get(59).toCharArray(), csvRecord.get(59).toCharArray());
+					Assert.assertArrayEquals(fileNameFive + " of " + runID + " does not match",
+							csvRecordToCompare.get(60).toCharArray(), csvRecord.get(60).toCharArray());
 
 					break;
 				}
